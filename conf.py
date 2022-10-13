1a7e7034d1cbdcfb5743cd3a5319574468d34a11
Olivier Bonaventure <Olivier.Bonaventure@uclouvain.be>
2022-10-13 18:54:21 +0200

    moved bytefield

--- conf.py
+++ conf.py
@@ -229,7 +229,7 @@ latex_elements = {
 
 # latex_additional_files
 
-latex_additional_files= [ '_templates/bytefield.sty' ]
+latex_additional_files= [ '_static/bytefield.sty' ]
 
 # If false, no module index is generated.
 #latex_use_modindex = True
